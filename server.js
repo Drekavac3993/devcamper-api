aa22f6fcc068854c041799e4b8a4d3f562f32dd9
Drekavac <diesel39931@gmail.com>
2020-05-14 18:00:54 +0600

    added custom error handler

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
+//App initialization
 const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//Hande unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
