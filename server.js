aec8d4539a79fae2180d08d26e8e1783ab64ef78
Drekavac <diesel39931@gmail.com>
2020-05-29 22:12:01 +0600

    added user model, and implemented login and register

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB();
 //Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 //App initialization
 const app = express();
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
