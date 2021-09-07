820472a4f74bd856ab3c28cd3a0ea14fb9a4de1a
Drekavac <diesel39931@gmail.com>
2021-09-07 17:52:43 +0600

    updated packages and fixed db connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
   });
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
