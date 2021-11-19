99b990f7d2c2bd479d1afa08190b63ac85a9503b
Drekavac <diesel39931@gmail.com>
2021-11-19 13:40:20 +0600

    fixed seeder

--- seeder.js
+++ seeder.js
@@ -1,40 +1,38 @@
-const fs = require("fs");
-const mongoose = require("mongoose");
-const colors = require("colors");
-const dotenv = require("dotenv");
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
 
 //Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 //Load models
-const Bootcamp = require("./models/Bootcamp");
-const Course = require("./models/Course");
-const User = require("./models/User");
-const Review = require("./models/Review");
+const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
+const User = require('./models/User');
+const Review = require('./models/Review');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
 });
 
 //Read JSON files
 const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
 const courses = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
 const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
 const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
 );
 
 //Imports into DB
@@ -44,7 +42,7 @@ const importData = async () => {
     await Course.create(courses);
     await User.create(users);
     await Review.create(reviews);
-    console.log("Data Imported...".green.inverse);
+    console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (error) {
     console.error(error);
@@ -58,15 +56,15 @@ const deleteData = async () => {
     await Course.deleteMany();
     await User.deleteMany();
     await Review.deleteMany();
-    console.log("Data Destroyed...".red.inverse);
+    console.log('Data Destroyed...'.red.inverse);
     process.exit();
   } catch (error) {
     console.error(error);
   }
 };
 
-if (process.argv[2] === "-i") {
+if (process.argv[2] === '-i') {
   importData();
-} else if (process.argv[2] === "-d") {
+} else if (process.argv[2] === '-d') {
   deleteData();
 }
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const colors = require("colors");
-const fileupload = require("express-fileupload");
-const cookieParser = require("cookie-parser");
-const mongoSanitize = require("express-mongo-sanitize");
-const helmet = require("helmet");
-const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
-const hpp = require("hpp");
-const cors = require("cors");
-const errorHandler = require("./middleware/error");
-const connectDB = require("./config/db");
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 //Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 //Connect to database
 connectDB();
 
 //Route files
-const bootcamps = require("./routes/bootcamps");
-const courses = require("./routes/courses");
-const auth = require("./routes/auth");
-const users = require("./routes/users");
-const reviews = require("./routes/reviews");
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 //App initialization
 const app = express();
@@ -37,8 +37,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Dev logging middleware
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 //File uploading
@@ -68,14 +68,14 @@ app.use(hpp());
 app.use(cors());
 
 //Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Mount routers
-app.use("/api/v1/bootcamps", bootcamps);
-app.use("/api/v1/courses", courses);
-app.use("/api/v1/auth", auth);
-app.use("/api/v1/users", users);
-app.use("/api/v1/reviews", reviews);
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
@@ -89,7 +89,7 @@ const server = app.listen(
 );
 
 //Hande unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
+process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red);
 
   //Close server and exit process
